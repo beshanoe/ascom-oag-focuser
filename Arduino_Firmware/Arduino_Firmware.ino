5df8410ef30819c1a80d637e0833189a3b6b0897
astroswell <maxvalenko@gmail.com>
2024-01-09 16:04:22 +0000

    protection from serial disconnect

--- Arduino_Firmware/Arduino_Firmware.ino
+++ Arduino_Firmware/Arduino_Firmware.ino
@@ -40,16 +40,14 @@ constexpr auto ERROR_INVALID_COMMAND = "ERROR:INVALID_COMMAND";
 volatile long targetPosition = 0;
 volatile long currentPosition = 0;
 unsigned long previousStepTime = 0;
-long stepIntervalMicros = 5000; // Adjust as per the stepper's speed rating.
+long stepIntervalMicros = 5000;  // Adjust as per the stepper's speed rating.
 bool shouldDisableWhenIdle = false;
 
-void setup()
-{
+void setup() {
   // Initialize serial port I/O.
   Serial.begin(115200);
-  while (!Serial)
-  {
-    ; // Wait for serial port to connect. Required for native USB!
+  while (!Serial) {
+    ;  // Wait for serial port to connect. Required for native USB!
   }
   Serial.flush();
 
@@ -67,140 +65,104 @@ void setup()
   targetPosition = currentPosition;
 }
 
-void loop()
-{
-  if (Serial.available() > 0)
-  {
+void loop() {
+  if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n');
-    if (command == COMMAND_PING)
-    {
+    if (command == COMMAND_PING) {
       handlePing();
-    }
-    else if (command == COMMAND_INFO)
-    {
+    } else if (command == COMMAND_INFO) {
       sendFirmwareInfo();
-    }
-    else if (command == COMMAND_FOCUSER_ISMOVING)
-    {
+    } else if (command == COMMAND_FOCUSER_ISMOVING) {
       sendFocuserState();
-    }
-    else if (command == COMMAND_FOCUSER_GETPOSITION)
-    {
+    } else if (command == COMMAND_FOCUSER_GETPOSITION) {
       sendFocuserPosition();
-    }
-    else if (command == COMMAND_FOCUSER_SETZEROPOSITION)
-    {
+    } else if (command == COMMAND_FOCUSER_SETZEROPOSITION) {
       setFocuserZeroPosition();
-    }
-    else if (command.startsWith(COMMAND_FOCUSER_MOVE))
-    {
+    } else if (command.startsWith(COMMAND_FOCUSER_MOVE)) {
       String arg = command.substring(strlen(COMMAND_FOCUSER_MOVE));
       int value = arg.toInt();
       serialPrintResult(command);
       moveFocuser(value);
-    }
-    else if (command == COMMAND_FOCUSER_HALT)
-    {
+    } else if (command == COMMAND_FOCUSER_HALT) {
       haltFocuser();
     }
     // Configuration commands
-    else if (command == COMMAND_CONFIG_GETMS1)
-    {
+    else if (command == COMMAND_CONFIG_GETMS1) {
       serialPrintResult(COMMAND_CONFIG_GETMS1);
       Serial.println(digitalRead(MS1_PIN) == HIGH ? TRUE : FALSE);
-    }
-    else if (command == COMMAND_CONFIG_GETMS2)
-    {
+    } else if (command == COMMAND_CONFIG_GETMS2) {
       serialPrintResult(COMMAND_CONFIG_GETMS2);
       Serial.println(digitalRead(MS2_PIN) == HIGH ? TRUE : FALSE);
-    }
-    else if (command == COMMAND_CONFIG_GETDISABLE_WHEN_IDLE)
-    {
+    } else if (command == COMMAND_CONFIG_GETDISABLE_WHEN_IDLE) {
       serialPrintResult(COMMAND_CONFIG_GETDISABLE_WHEN_IDLE);
       Serial.println(shouldDisableWhenIdle ? TRUE : FALSE);
-    }
-    else if (command == COMMAND_CONFIG_GET_STEP_INTERVAL)
-    {
+    } else if (command == COMMAND_CONFIG_GET_STEP_INTERVAL) {
       serialPrintResult(COMMAND_CONFIG_GET_STEP_INTERVAL);
       Serial.println(stepIntervalMicros);
-    }
-    else if (command.startsWith(COMMAND_CONFIG_SETMS1))
-    {
+    } else if (command.startsWith(COMMAND_CONFIG_SETMS1)) {
       // arg as TRUE or FALSE
       String arg = command.substring(strlen(COMMAND_CONFIG_SETMS1));
       digitalWrite(MS1_PIN, arg == TRUE ? HIGH : LOW);
       serialPrintResult(command);
       Serial.println(OK);
-    }
-    else if (command.startsWith(COMMAND_CONFIG_SETMS2))
-    {
+    } else if (command.startsWith(COMMAND_CONFIG_SETMS2)) {
       // arg as TRUE or FALSE
       String arg = command.substring(strlen(COMMAND_CONFIG_SETMS2));
       digitalWrite(MS2_PIN, arg == TRUE ? HIGH : LOW);
       serialPrintResult(command);
       Serial.println(OK);
-    }
-    else if (command.startsWith(COMMAND_CONFIG_DISABLE_WHEN_IDLE))
-    {
+    } else if (command.startsWith(COMMAND_CONFIG_DISABLE_WHEN_IDLE)) {
       // arg as TRUE or FALSE
       String arg = command.substring(strlen(COMMAND_CONFIG_DISABLE_WHEN_IDLE));
       shouldDisableWhenIdle = arg == TRUE;
-      if (!isMoving())
-      {
+      if (!isMoving()) {
         digitalWrite(ENABLED_PIN, shouldDisableWhenIdle ? HIGH : LOW);
       }
       serialPrintResult(command);
       Serial.println(OK);
-    }
-    else if (command.startsWith(COMMAND_CONFIG_SET_STEP_INTERVAL))
-    {
+    } else if (command.startsWith(COMMAND_CONFIG_SET_STEP_INTERVAL)) {
       // arg as microseconds
       String arg = command.substring(strlen(COMMAND_CONFIG_SET_STEP_INTERVAL));
       stepIntervalMicros = arg.toInt();
       serialPrintResult(command);
       Serial.println(OK);
-    }
-    else
-    {
+    } else {
       handleInvalidCommand();
     }
   }
 
   step();
+
+  if (!Serial) {
+    stop();
+  }
 }
 
-void step()
-{
-  if (targetPosition == currentPosition)
-  {
+void step() {
+  if (targetPosition == currentPosition) {
     stop();
     return;
   }
 
   unsigned long currentMicros = micros();
-  if (currentMicros - previousStepTime >= stepIntervalMicros)
-  {
-    if (targetPosition > currentPosition)
-    {
+  if (currentMicros - previousStepTime >= stepIntervalMicros) {
+    if (targetPosition > currentPosition) {
       digitalWrite(DIR_PIN, LOW);
       currentPosition++;
-    }
-    else
-    {
+    } else {
       digitalWrite(DIR_PIN, HIGH);
       currentPosition--;
     }
 
     digitalWrite(STEP_PIN, HIGH);
-    delayMicroseconds(1); // A very short pulse.
+    delayMicroseconds(1);  // A very short pulse.
     digitalWrite(STEP_PIN, LOW);
 
     previousStepTime = currentMicros;
   }
 }
 
-void stop()
-{
+void stop() {
   // Make sure we don't take another step.
   targetPosition = currentPosition;
 
@@ -208,83 +170,67 @@ void stop()
   // EEPROM.put(EEPROM_POSITION_BASE_ADDR, position);
 
   // And de-energize the stepper, prevent heat build up, and eliminate vibrations.
-  if (shouldDisableWhenIdle)
-  {
+  if (shouldDisableWhenIdle) {
     digitalWrite(ENABLED_PIN, HIGH);
   }
 }
 
-void serialPrintResult(String command)
-{
+void serialPrintResult(String command) {
   Serial.print("RESULT:");
   Serial.print(command);
   // check if command ends with a colon
-  if (command[command.length() - 1] != ':')
-  {
+  if (command[command.length() - 1] != ':') {
     Serial.print(":");
   }
 }
 
 //-- FOCUSER HANDLING ------------------------------------------------------
 
-bool isMoving()
-{
+bool isMoving() {
   return abs(targetPosition - currentPosition) > 0;
 }
 
-void sendFocuserState()
-{
+void sendFocuserState() {
   serialPrintResult(COMMAND_FOCUSER_ISMOVING);
   Serial.println(isMoving() ? TRUE : FALSE);
 }
 
-void sendFocuserPosition()
-{
+void sendFocuserPosition() {
   serialPrintResult(COMMAND_FOCUSER_GETPOSITION);
   Serial.println(currentPosition);
 }
 
-void setFocuserZeroPosition()
-{
+void setFocuserZeroPosition() {
   serialPrintResult(COMMAND_FOCUSER_SETZEROPOSITION);
-  if (!isMoving())
-  {
+  if (!isMoving()) {
     currentPosition = DEFAULT_POSITION;
     // EEPROM.put(EEPROM_POSITION_BASE_ADDR, position);
     Serial.println(OK);
-  }
-  else
-  {
+  } else {
     // Cannot set zero position while focuser is still moving...
     Serial.println(NOK);
   }
 }
 
-void moveFocuser(int target_position)
-{
-  if (isMoving())
-  {
+void moveFocuser(int target_position) {
+  if (isMoving()) {
     // Cannot move while focuser is still moving from previous request...
     Serial.println(NOK);
     return;
   }
 
-  if (target_position >= 0)
-  {
+  if (target_position >= 0) {
     // Enable the stepper.
     digitalWrite(ENABLED_PIN, LOW);
     targetPosition = target_position;
     Serial.println(OK);
-  }
-  else
-  {
+  } else {
     // Cannot move to a negative position...
     Serial.println(NOK);
   }
 }
 
-void haltFocuser()
-{
+void haltFocuser() {
   stop();
   serialPrintResult(COMMAND_FOCUSER_HALT);
   Serial.println(OK);
@@ -292,19 +238,16 @@ void haltFocuser()
 
 //-- MISCELLANEOUS ------------------------------------------------------------
 
-void handlePing()
-{
+void handlePing() {
   serialPrintResult(COMMAND_PING);
   Serial.println(DEVICE_GUID);
 }
 
-void sendFirmwareInfo()
-{
+void sendFirmwareInfo() {
   serialPrintResult(COMMAND_INFO);
   Serial.println(RESULT_INFO);
 }
 
-void handleInvalidCommand()
-{
+void handleInvalidCommand() {
   Serial.println(ERROR_INVALID_COMMAND);
 }
